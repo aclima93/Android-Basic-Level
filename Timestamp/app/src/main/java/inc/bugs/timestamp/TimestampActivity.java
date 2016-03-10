2bf950a2484b65d15463a8f78672001de908d3e9
António Lima <tocalagarto93@gmail.com>
2016-03-10 15:18:44 +0000

    logging timestamp upon pause

--- Timestamp/app/src/main/java/inc/bugs/timestamp/TimestampActivity.java
+++ Timestamp/app/src/main/java/inc/bugs/timestamp/TimestampActivity.java
@@ -1,7 +1,12 @@
 package inc.bugs.timestamp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class TimestampActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class TimestampActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timestamp);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "Welcome back!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        TextView timestamp = (TextView) findViewById(R.id.timestamp_tv);
+        timestamp.setText("Last log: " + DateFormat.getDateTimeInstance().format(new Date()));
+
+        Toast.makeText(getApplicationContext(), "Be back later", Toast.LENGTH_SHORT).show();
     }
 }
